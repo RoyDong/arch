b2e5c456cbe7a37a33380b52842e17becd104379
Roy Dong <roy.dongwei@gmail.com>
2012-05-11 11:32:24 +0800

    refact

--- system/kernel.php
+++ system/kernel.php
@@ -18,30 +18,22 @@ function main(){
         }catch( Exception $e ){
             $action->error( $e->getMessage() , $e->getCode() );
         }
-    }else{
-        require ROOT_DIR . '/web/404.html';
-    }
+    }else
+        require ROOT_DIR . '/public/404.html';
 }
 
 function c( $key = 'all' , $filename = 'config' ){
-    static $config = array( );
-    $keys = explode( '.' , $key );
+    static $config = array();
 
     if( empty( $config[ $filename ] ) ){
         $file = ROOT_DIR . '/config/' . $filename . '.php';
-
-        if( file_exists( $file ) ){
-            $config[ $filename ] = require $file;
-        }
+        if( file_exists( $file ) ) $config[ $filename ] = require $file;
     }
 
-    if( isset( $config[ $filename ][ $key ] ) ){
+    if( $key === 'all' ) return $config[ $filename ];
+
+    if( isset( $config[ $filename ][ $key ] ) )
         return $config[ $filename ][ $key ];
-    }
-
-    if( $key === 'all' ){
-        return $config[ $filename ];
-    }
 }
 
 function t( $text , $params = array( ) , $package = 'core' ){
@@ -50,16 +42,13 @@ function t( $text , $params = array( ) , $package = 'core' ){
     if( empty( $i18n[ $package ] ) ){
         $file = ROOT_DIR . '/i18n/' . $package . '.php';
 
-        if( file_exists( $file ) ){
+        if( file_exists( $file ) )
             $i18n[ $package ] = require $file;
-        }else{
+        else
             return $text;
-        }
     }
 
-    if( empty( $i18n[ $package ][ $text ] ) ){
-        return $text;
-    }
+    if( empty( $i18n[ $package ][ $text ] ) ) return $text;
 
     $sentence = $i18n[ $package ][ $text ];
 
@@ -68,7 +57,7 @@ function t( $text , $params = array( ) , $package = 'core' ){
 
         foreach( $params as $key => $value ){
             $key = '{' . $key . '}';
-            $keys[ ] = $key;
+            $keys[] = $key;
             $replaces[ $key ] = $value;
         }
 
@@ -79,29 +68,23 @@ function t( $text , $params = array( ) , $package = 'core' ){
 }
 
 function __autoload( $className ){
-    $type = substr( $className , -5 );
-    $dir = $type === 'Model' ? '/models/' : '/lib/';
+    $type = substr( $className , -4 );
+    $dir = $type === 'Data' ? '/data/' : '/model/';
     $file = ROOT_DIR . $dir . $className . '.php';
 
-    if( file_exists( $file ) ){
-        require $file;
-    }
+    if( file_exists( $file ) ) require $file;
 }
 
 abstract class Action{
 
     protected $layout = 'main';
 
-    public function init(){
-        
-    }
+    public function init(){}
 
-    public function filter(){
-        
-    }
+    public function filter(){}
 
     public function error( $message , $code ){
-        require ROOT_DIR . '/templates/error.php';
+        require ROOT_DIR . '/template/error.php';
     }
 
     abstract function execute();
@@ -114,9 +97,8 @@ abstract class Action{
      */
     protected function get( $key , $default = null ){
         if( empty( $_GET[ $key ] ) ){
-            if( $default === null ){
+            if( $default === null )
                 throw new KernelException( t( 'param missing' , array( 'param' => $key ) ) , KernelException::PARAM_MISSING );
-            }
 
             return $default;
         }
@@ -132,9 +114,8 @@ abstract class Action{
      */
     protected function post( $key , $default = null ){
         if( empty( $_POST[ $key ] ) ){
-            if( $default === null ){
+            if( $default === null )
                 throw new KernelException( t( 'param missing' , array( 'param' => $key ) ) , KernelException::PARAM_MISSING );
-            }
 
             return $default;
         }
@@ -143,7 +124,7 @@ abstract class Action{
     }
 
     protected function tpl( $path ){
-        return ROOT_DIR . '/templates/' . $path . '.php';
+        return ROOT_DIR . '/template/' . $path . '.php';
     }
 
     protected function render( $contentTemplatePath , $data = array() ){
@@ -156,27 +137,17 @@ abstract class Action{
     }
 
     protected function createUrl( $url ){
-        if( strncmp( $url , 'http://' , 7 ) === 0 ){
+        if( strncmp( $url , 'http://' , 7 ) === 0 )
             return $url;
-        }
 
         return '/' . $url;
     }
 }
 
-class KernelException extends Exception{
-
-    const PARAM_MISSING = 10;
-    const CONFIG_MISSING = 11;
-    const SERVER_BUSY = 20;
-    const CACHE_DISCONNECTED = 21;
-
-}
-
 /**
  * @author Roy
  */
-class Model{
+class Data{
 
     /**
      * singleton object
@@ -196,10 +167,10 @@ class Model{
     protected $cache;
 
     /**
-     * data namespace used as table name or cache key prefix
+     * data tableName used as table name or cache key prefix
      * @var string
      */
-    protected $namespace;
+    protected $tableName;
 
     /**
      * get single object of a data class
@@ -237,16 +208,15 @@ class Model{
             $this->cache = new Redis;
             $config = c( 'cache' );
 
-            if( !$this->cache->pconnect( $config[ 'host' ] , $config[ 'port' ] ) ){
+            if( !$this->cache->pconnect( $config[ 'host' ] , $config[ 'port' ] ) )
                 throw new KernelException( t( 'cache disconnected' ) , KernelException::CACHE_DISCONNECTED );
-            }
         }
 
         return $this->cache;
     }
 
     protected function cacheKey( $uniqueId ){
-        return $this->namespace . '-' . $uniqueId;
+        return $this->tableName . '-' . $uniqueId;
     }
 
     /**
@@ -258,11 +228,11 @@ class Model{
         $columns = $values = array( );
 
         foreach( $data as $column => $value ){
-            $columns[ ] = $column;
-            $values[ ] = $value;
+            $columns[] = $column;
+            $values[] = $value;
         }
 
-        $this->initPdo()->exec( 'insert into `' . $this->namespace . '` (`' . implode( '`,`' , $columns ) . '`) values ("' . implode( '","' , $values ) . '")' );
+        $this->initPdo()->exec( 'insert into `' . $this->tableName . '` (`' . implode( '`,`' , $columns ) . '`) values ("' . implode( '","' , $values ) . '")' );
         return $this->pdo->lastInsertId();
     }
 
@@ -274,35 +244,35 @@ class Model{
     public function update( $data , $where ){
         $tmp = '';
 
-        foreach( $row[ 'data' ] as $column => $value ){
+        foreach( $data as $column => $value ){
             $tmp .= '`' . $column . '`="' . $value . '",';
         }
 
-        return $this->initPdo()->exec( 'update `' . $this->namespace . '` set ' . substr( $tmp , 0 , -1 ) . ' where ' . $row[ 'where' ] );
+        return $this->initPdo()->exec( 'update `' . $this->tableName . '` set ' . substr( $tmp , 0 , -1 ) . ' where ' . $where );
     }
 
     protected function delete( $where , $limit = '0,1' ){
-        return $this->initPdo()->exec( 'delete * from `' . $this->namespace . '` where ' . $where . ' ' . $limit );
+        return $this->initPdo()->exec( 'delete * from `' . $this->tableName . '` where ' . $where . ' ' . $limit );
     }
 
     /**
      * get data from sql db
      * @return array
      */
-    public function fetch( $where ){
-        return $this->initPdo()->query( 'select * from `' . $this->namespace . '` where ' . $where . ' limit 0,1' )->fetch( PDO::FETCH_ASSOC );
+    public function read( $where ){
+        return $this->initPdo()->query( 'select * from `' . $this->tableName . '` where ' . $where . ' limit 0,1' )->fetch( PDO::FETCH_ASSOC );
     }
 
     /**
      * find multi rows from sql db
      * @return array
      */
-    public function fetchAll( $where , $order = '' , $limit = '' ){
-        return $this->initPdo()->query( 'select * from `' . $this->namespace . '` where ' . $where . ' ' . $order . ' ' . $limit )->fetchAll( PDO::FETCH_ASSOC );
+    public function readAll( $where , $order = '' , $limit = '' ){
+        return $this->initPdo()->query( 'select * from `' . $this->tableName . '` where ' . $where . ' ' . $order . ' ' . $limit )->fetchAll( PDO::FETCH_ASSOC );
     }
 
     public function count( $where = '1=1' ){
-        $row = $this->initPdo()->query( 'select count(*) c from `' . $this->namespace . '` where ' . $where )->fetch( PDO::FETCH_ASSOC );
+        $row = $this->initPdo()->query( 'select count(*) c from `' . $this->tableName . '` where ' . $where )->fetch( PDO::FETCH_ASSOC );
         return $row[ 'c' ];
     }
 
