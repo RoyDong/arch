dd49960dec717a9a0522f1da3c27af18986f821b
Roy Dong <roy.dongwei@gmail.com>
2012-07-08 08:44:35 +0800

    fix bug

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 $(function(){
     var forms = $('#forms');
 
-    $('#signin-form-btn').click(function(){
+    $('#signin-form-btn').click(function(e){
         forms.animate({ 'margin-top': '-390px' } , 500 , function(){
             $( '#signin-email' ).focus();
         });
     });
 
-    $('#signup-form-btn').click(function(){
+    $('#signup-form-btn').click(function(e){
         forms.animate({ 'margin-top': '0px' } , 500 , function(){
             $( '#signup-email' ).focus();
         });
